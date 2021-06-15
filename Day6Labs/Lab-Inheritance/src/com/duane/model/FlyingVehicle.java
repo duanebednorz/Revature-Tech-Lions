abb37ab7df058315b5b7f13e7a86821f509a9fbf
Duane Bednorz <duanebednorz@aggienetwork.com>
2021-06-14 22:41:57 -0500

    day6

---
+++ Day6Labs/Lab-Inheritance/src/com/duane/model/FlyingVehicle.java
@@ -0,0 +1,14 @@
+package com.duane.model;
+
+public class FlyingVehicle extends Vehicle{
+	
+	private short wings;
+    private Engine engine;
+    
+    public FlyingVehicle(){}
+
+    public FlyingVehicle(short wings, Engine engine){
+        this.wings = wings;
+        this.engine = engine;
+    }
+}
